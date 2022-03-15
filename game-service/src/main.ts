98f5880dfd6f8c6867e2c5d3e6e4ccd71a29df3f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-03-15 11:38:17 +0100

    feat: automatically run migrations if needed

--- game-service/src/main.ts
+++ game-service/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MikroORM } from '@mikro-orm/core';
+import mikroConfig from './mikro-orm.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Run the migrations automatically.
+  const orm = await MikroORM.init(mikroConfig);
+  const migrator = orm.getMigrator();
+  const migrations = await migrator.getPendingMigrations();
+
+  if (migrations && migrations.length > 0) {
+    await migrator.up();
+  }
+
   await app.listen(3000);
 }
 
