04622cf52a179c9bc6b393de50a184679fc9a2b7
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-03-15 11:06:54 +0100

    fix: mikro-orm configuration and format files

--- game-service/src/lobby/lobby.service.ts
+++ game-service/src/lobby/lobby.service.ts
@@ -1,23 +1,21 @@
-import { Injectable } from "@nestjs/common";
-import { InjectRepository } from "@mikro-orm/nestjs"
-import { EntityRepository } from "@mikro-orm/postgresql"
-import { Lobby } from "./lobby.entity";
-import { CreateLobby } from "./dto/create-lobby.dto";
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository } from '@mikro-orm/postgresql';
+import { Lobby } from './lobby.entity';
+import { CreateLobby } from './dto/create-lobby.dto';
 
 @Injectable()
 export class LobbyService {
-    constructor(
-        @InjectRepository(Lobby)
-        private readonly lobbyRepository: EntityRepository<Lobby>
-    ) {}
+  constructor(
+    @InjectRepository(Lobby)
+    private readonly lobbyRepository: EntityRepository<Lobby>,
+  ) {}
 
-    getAll(): Promise<Lobby[]> {
-        return this.lobbyRepository.findAll();
-    }
+  getAll(): Promise<Lobby[]> {
+    return this.lobbyRepository.findAll();
+  }
 
-    createNewLobby(settings: CreateLobby): Lobby {
-        let lobby = this.lobbyRepository.create(settings);
-
-        return lobby;
-    }
+  createNewLobby(settings: CreateLobby): Lobby {
+    return this.lobbyRepository.create(settings);
+  }
 }
