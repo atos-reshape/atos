49f11bfb76fbf2a65c974e5cd8df485f427b4753
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-03-15 11:45:10 +0100

    fix: .env configuration in nest

--- game-service/src/app.module.ts
+++ game-service/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { LobbiesModule } from './lobby/lobby.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), LobbiesModule],
+  imports: [MikroOrmModule.forRoot(), ConfigModule.forRoot(), LobbiesModule],
   controllers: [],
   providers: [],
 })
