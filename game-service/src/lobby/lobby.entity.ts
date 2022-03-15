59621225ff7d319b98a8f3b85ebce21644dbe7ff
Stijn Nooijen <s.j.m.nooijen@gmail.com>
2022-03-15 15:35:46 +0100

    fix: Fix usage of repo and change id to string

--- game-service/src/lobby/lobby.entity.ts
+++ game-service/src/lobby/lobby.entity.ts
@@ -1,16 +1,22 @@
 import { Entity, Index, Property, Unique } from '@mikro-orm/core';
 import { BaseEntity } from '../database/entities/base-entity.entity';
 
+function generateGameCode() {
+  let text = '';
+  const possible = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  for (let i = 0; i < 8; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
 @Entity()
 export class Lobby extends BaseEntity {
   @Property()
   @Unique()
   @Index()
-  code!: string;
+  code: string = generateGameCode();
 
   @Property()
   title!: string;
-
-  @Property()
-  cardSetId?: string;
 }
