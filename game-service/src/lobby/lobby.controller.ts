3d027f83e72c4101b27d99722701e94d15b1ec84
StijnKing <s.j.m.nooijen@gmail.com>
2022-03-19 18:24:42 +0100

    feat: Add round entity with create and get methods

--- game-service/src/lobby/lobby.controller.ts
+++ game-service/src/lobby/lobby.controller.ts
@@ -1,18 +1,24 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { LobbyService } from './lobby.service';
-import { CreateLobby } from './dto/create-lobby.dto';
+import { CreateLobbyDto } from './dto/create-lobby.dto';
+import { Lobby } from './lobby.entity';
 
 @Controller('lobbies')
 export class LobbyController {
-  constructor(private readonly lobbiesService: LobbyService) {}
+  constructor(private readonly lobbyService: LobbyService) {}
 
   @Get()
-  getLobbies(): Promise<any[]> {
-    return this.lobbiesService.getAll();
+  getLobbies(): Promise<Lobby[]> {
+    return this.lobbyService.getAll();
+  }
+
+  @Get(':id')
+  getLobby(@Param('id') id: string): Promise<Lobby> {
+    return this.lobbyService.getById(id);
   }
 
   @Post()
-  createLobby(@Body() params: CreateLobby): any {
-    return this.lobbiesService.createNewLobby(params);
+  createLobby(@Body() params: CreateLobbyDto): Promise<Lobby> {
+    return this.lobbyService.createNewLobby(params);
   }
 }
---
+++ game-service/src/round/round.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Param,
+  Post,
+  UseInterceptors,
+} from '@nestjs/common';
+import { RoundService } from './round.service';
+import { CreateRoundDto } from './dto/create-round.dto';
+import { RoundResponseDto } from './dto/round-response.dto';
+
+@Controller('lobbies/:lobby_id/rounds')
+@UseInterceptors(ClassSerializerInterceptor)
+export class RoundController {
+  constructor(private readonly roundService: RoundService) {}
+
+  @Get()
+  async getRounds(
+    @Param('lobby_id') lobby_id: string,
+  ): Promise<RoundResponseDto[]> {
+    const rounds = await this.roundService.getAllForLobby(lobby_id);
+    return rounds.map((round) => new RoundResponseDto(round));
+  }
+
+  @Post()
+  async createRound(
+    @Param('lobby_id') lobby_id: string,
+    @Body() params: CreateRoundDto,
+  ): Promise<RoundResponseDto> {
+    const round = await this.roundService.createNewRoundForLobby(
+      lobby_id,
+      params,
+    );
+    return new RoundResponseDto(round);
+  }
+}
