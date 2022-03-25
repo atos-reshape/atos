da1184ec5d00a5e31fc36325affb24d959186789
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-03-25 22:25:41 +0100

    fix: prevent open connections

--- admin-service/src/main.ts
+++ admin-service/src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.enableShutdownHooks();
 
   const config = new DocumentBuilder()
     .setTitle('Admin service API')
