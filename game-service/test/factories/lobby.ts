8f2bebbb21c97a930e1e1971f695e889d2bf73ac
Stijn Nooijen <s.j.m.nooijen@gmail.com>
2022-03-25 16:57:52 +0100

    feat: Add integrated tests and test coverage

--- game-service/test/factories/lobby.ts
+++ game-service/test/factories/lobby.ts
@@ -1,10 +1,11 @@
 import { v4 } from 'uuid';
 import { faker } from '@faker-js/faker';
 import { Lobby } from '../../src/lobby/lobby.entity';
-import generateGameCode from '../../src/helpers/gameCodeGenerator.helper';
+import { generateGameCode } from '@helpers/index';
+import { MikroORM } from '@mikro-orm/core';
 
-export const lobby = (data: Partial<Lobby> = {}): Lobby => {
-  return {
+export const lobby = (data: Partial<Lobby> = {}, orm?: MikroORM): Lobby => {
+  let lobby = {
     id: v4(),
     code: generateGameCode(),
     createdAt: faker.date.recent(),
@@ -12,4 +13,24 @@ export const lobby = (data: Partial<Lobby> = {}): Lobby => {
     title: faker.lorem.sentence(5),
     ...data,
   } as Lobby;
+
+  if (!!orm) {
+    lobby = orm.em.create(Lobby, lobby);
+    orm.em.persist(lobby);
+  }
+
+  return lobby;
+};
+
+export const createLobbies = (
+  lobbies: Partial<Lobby>[] = [],
+  orm?: MikroORM,
+): Lobby[] => {
+  const result = lobbies.map((l: Partial<Lobby>) => lobby(l, orm));
+
+  if (!!orm) {
+    result.forEach((l) => orm.em.persist(l));
+  }
+
+  return result;
 };
