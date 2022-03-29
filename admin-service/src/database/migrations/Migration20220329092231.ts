348195416a3442c696a3b6d46e80238bbf70411f
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-03-29 14:38:53 +0200

    chore: run formatting on migrations

--- admin-service/src/database/migrations/Migration20220329092231.ts
+++ admin-service/src/database/migrations/Migration20220329092231.ts
@@ -1,13 +1,18 @@
 import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20220329092231 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('create table "tag" ("id" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null);');
-    this.addSql('alter table "tag" add constraint "tag_pkey" primary key ("id");');
+    this.addSql(
+      'create table "tag" ("id" varchar(255) not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "name" varchar(255) not null);',
+    );
+    this.addSql(
+      'alter table "tag" add constraint "tag_pkey" primary key ("id");',
+    );
 
     this.addSql('alter table "card" add column "tag_id" varchar(255) null;');
-    this.addSql('alter table "card" add constraint "card_tag_id_foreign" foreign key ("tag_id") references "tag" ("id") on update cascade on delete set null;');
+    this.addSql(
+      'alter table "card" add constraint "card_tag_id_foreign" foreign key ("tag_id") references "tag" ("id") on update cascade on delete set null;',
+    );
   }
 
   async down(): Promise<void> {
@@ -17,5 +22,4 @@ export class Migration20220329092231 extends Migration {
 
     this.addSql('alter table "card" drop column "tag_id";');
   }
-
 }
