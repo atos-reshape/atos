551c9a430cdbfd8ac33e796636d27ddf33326a50
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2022-04-01 10:15:41 +0300

    swagger api doc updated

--- admin-service/src/card-set/card-set.controller.ts
+++ admin-service/src/card-set/card-set.controller.ts
@@ -8,7 +8,7 @@ import {
   Put,
   Query,
 } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiQuery, ApiOkResponse, ApiNotFoundResponse, ApiConflictResponse, ApiBadRequestResponse } from '@nestjs/swagger';
 import { CardSetService } from './card-set.service';
 import { CreateCardSetDto } from './dtos/create-card-set.dto';
 import { CardSet } from './entities/card-set.entity';
@@ -20,34 +20,33 @@ export class CardSetController {
 
   @ApiOperation({ summary: 'Get all card sets' })
   @ApiQuery({ name: 'isActive' })
-  @ApiResponse({ status: 200, description: 'Returned all card sets' })
+  @ApiOkResponse()
   @Get()
   async findAll(@Query('isActive') isActive = true): Promise<CardSet[]> {
     return this.cardSetService.findAll(isActive);
   }
 
   @ApiOperation({ summary: 'Get a card set by id' })
-  @ApiResponse({ status: 200, description: 'Returned card set by id' })
-  @ApiResponse({ status: 404, description: 'Card set not found' })
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<CardSet> {
     return this.cardSetService.findOne(id);
   }
 
-  @ApiOperation({ summary: 'Create card set' })
-  @ApiResponse({ status: 201, description: 'Card set created' })
-  @ApiResponse({ status: 400, description: 'Invalid card set' })
-  @ApiResponse({ status: 409, description: 'Card set already exists' })
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
+  @ApiConflictResponse()
   @Post()
   async create(@Body() card: CreateCardSetDto): Promise<CardSet> {
     return this.cardSetService.create(card);
   }
 
   @ApiOperation({ summary: 'Update card set' })
-  @ApiResponse({ status: 200, description: 'Card set updated' })
-  @ApiResponse({ status: 400, description: 'Invalid card set' })
-  @ApiResponse({ status: 404, description: 'Card set not found' })
-  @ApiResponse({ status: 409, description: 'Card set already exists' })
+  @ApiOkResponse()
+  @ApiBadRequestResponse()
+  @ApiNotFoundResponse()
+  @ApiConflictResponse()
   @Put(':id')
   async update(
     @Param('id') id: string,
@@ -57,8 +56,8 @@ export class CardSetController {
   }
 
   @ApiOperation({ summary: 'Delete card set by id' })
-  @ApiResponse({ status: 200, description: 'Card set deleted' })
-  @ApiResponse({ status: 404, description: 'Card set not found' })
+  @ApiOkResponse()
+  @ApiNotFoundResponse()
   @Delete(':id')
   async delete(@Param('id') id: string): Promise<void> {
     return this.cardSetService.delete(id);
