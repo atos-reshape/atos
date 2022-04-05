0787702faa8ed1ac9073686ddd55e38047489f7a
Rares Petrisor <rares.petrisor1@gmail.com>
2022-04-05 16:21:51 +0200

    feat: Added Drag and Drop in /game screen

--- web/src/pages/GameView/GameView.tsx
+++ web/src/pages/GameView/GameView.tsx
@@ -14,6 +14,29 @@ function GameView() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get('id');
 
+  const { data } = useGetCards({
+    onError: () => alert('Could not fetch cards')
+  });
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
+
   return (
     <DndProvider backend={HTML5Backend}>
       <SocketProvider id={id}>
