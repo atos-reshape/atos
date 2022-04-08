102e4d2438d6d729e1005c319805166a5381ac3d
David Copoeru <copoerudavid25@gmail.com>
2022-04-08 11:19:04 +0200

    linter errors fixup

--- admin-service/src/main.ts
+++ admin-service/src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule, { cors: true});
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
