8223aec41f8fe4e0e5544fdc64e138096be60635
David Copoeru <copoerudavid25@gmail.com>
2022-04-08 15:15:36 +0200

    removed changes to prototype folder

--- prototype/src/App.tsx
+++ prototype/src/App.tsx
@@ -4,8 +4,6 @@ import Quizzes from './quizzes.json';
 import AppNavbar from './components/navbar/AppNavbar';
 import AppHeader from './components/header/AppHeader';
 import Quiz from './components/quiz/Quiz';
-import CardInputs from './components/input/Input';
-import CardUpdates from './components/update/UpdateCard';
 import QuizView from './components/quiz/QuizView';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -19,7 +17,6 @@ export default function App(): JSX.Element {
     <BrowserRouter>
       <Routes>
         <Route index element={<QuizView />} />
-        <Route path="/cards" element={<CardUpdates />} />
       </Routes>
     </BrowserRouter>
   );
