594359cf23a2d69ecd272a34f609b8fee1730e2c
Rares Petrisor <rares.petrisor1@gmail.com>
2022-04-12 14:57:13 +0200

    feat: Added Tinder-like carousel

--- web/src/components/Carousel/Carousel.tsx
+++ web/src/components/Carousel/Carousel.tsx
@@ -14,15 +14,15 @@ function Carousel() {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 1
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 1
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 3
+      items: 1
     },
     mobile: {
       breakpoint: { max: 564, min: 0 },
