795e88b7266ebe24fbc254ac744178e2e19876d3
StijnKing <s.j.m.nooijen@gmail.com>
2022-04-12 14:49:30 +0200

    feat: Add strategy for Microsoft AD with SSO as a proof of concept

--- admin-service/src/app.module.ts
+++ admin-service/src/app.module.ts
@@ -4,9 +4,15 @@ import { MikroORM } from '@mikro-orm/core';
 import { CardModule } from './card/card.module';
 import { CardSetModule } from './card-set/card-set.module';
 import { TagModule } from './tag/tag.module';
+import {
+  ActiveDirectoryStrategy,
+  AppController,
+} from './auth/active-directory.strategy';
 
 @Module({
   imports: [MikroOrmModule.forRoot(), CardModule, CardSetModule, TagModule],
+  providers: [ActiveDirectoryStrategy],
+  controllers: [AppController],
 })
 export class AppModule implements OnModuleInit {
   constructor(private readonly orm: MikroORM) {}
