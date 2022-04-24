45ce8468e97828d34aed7278cc66e4282e7ab0ee
StijnKing <s.j.m.nooijen@gmail.com>
2022-04-24 14:21:21 +0200

    feat: Add support for HTTPS certificates

--- admin-service/src/main.ts
+++ admin-service/src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('../localhost.key'),
+    cert: fs.readFileSync('../localhost.crt'),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
