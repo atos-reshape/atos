f7bf3729594e15c330f1e8c13d0d5a7f7307c125
Jelle Huibregtse <42927362+jellehuibregtse@users.noreply.github.com>
2022-05-13 15:00:14 +0200

    feat: add dark mode for the admin application

--- admin/src/appbar/CustomAppBar.tsx
+++ admin/src/appbar/CustomAppBar.tsx
@@ -1,14 +1,31 @@
-import { LocalesMenuButton, AppBar, AppBarProps } from 'react-admin';
-import { Typography } from '@mui/material';
+import {
+  AppBar,
+  AppBarProps,
+  defaultTheme,
+  LocalesMenuButton,
+  ToggleThemeButton,
+} from 'react-admin';
+import { Box, createTheme, Typography } from '@mui/material';
 
-export const CustomAppBar = (props: AppBarProps) => (
-  <AppBar {...props}>
-    <Typography flex="1" variant="h6" id="react-admin-title"></Typography>
-    <LocalesMenuButton
-      languages={[
-        { locale: 'en', name: 'English' },
-        { locale: 'nl', name: 'Nederlands' },
-      ]}
-    />
-  </AppBar>
-);
+export const CustomAppBar = (props: AppBarProps) => {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  return (
+    <AppBar {...props}>
+      <Box flex="1">
+        <Typography flex="1" variant="h6" id="react-admin-title" />
+      </Box>
+      <LocalesMenuButton
+        languages={[
+          { locale: 'en', name: 'English' },
+          { locale: 'nl', name: 'Nederlands' },
+        ]}
+      />
+      <ToggleThemeButton lightTheme={defaultTheme} darkTheme={darkTheme} />
+    </AppBar>
+  );
+};
