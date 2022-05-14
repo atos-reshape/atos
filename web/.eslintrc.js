06aa6e36fbd43cbe36cd092a781935b88106832e
VladimirCVB <47753522+VladimirCVB@users.noreply.github.com>
2022-05-14 17:58:13 +0300

    Loading Screen restyle

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
-    'plugin:prettier/recommended'
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
